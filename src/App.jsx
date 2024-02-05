99e74d9115bbb63c1ed1bb6db459ed0b53ba3e42
Chxshire22 <akuindra22@gmail.com>
2024-02-05 10:48:15 +0800

    deleted boilerplt, setup tailwind,dayjs,router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import logo from "/logo.png";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-      <h1>Project 3</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     </>
   );
 }
