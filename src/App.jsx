801b9dc2f2665471ad9a73096a8c1b7da571fbf0
Chxshire22 <akuindra22@gmail.com>
2024-02-05 12:16:00 +0800

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,69 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Components/Home";
+import Profile from "./Components/Profile";
+import Explore from "./Components/Explore";
+import EditProfile from "./Components/EditProfile";
+import Chats from "./Components/Chats";
+import Chatroom from "./Components/Chatroom";
+import AddListing from "./Components/AddListing";
+import Listing from "./Components/Listing";
+import Likes from "./Components/Likes";
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Home />,
+		},
+		{
+			path: `/profile/`,
+			//will need the unique ID of profile
+			children: [
+				{
+					path: ":uid",
+					element: <Profile />,
+				},
+			],
+		},
+		{
+			path: "/chats",
+			element: <Chats />,
+		},
+		{
+			path: "/chat/",
+			children: [
+				{
+					path: ":uid",
+					element: <Chatroom />,
+				},
+			],
+		},
+		{
+			path: "/likes",
+			element: <Likes />,
+		},
+		//listing here will need a path that directs it to individual listing via listingId
+		{
+			path: "/listing/",
+			children: [
+				{
+					path: ":uid",
+					element: <Listing />,
+				},
+			],
+		},
+		{
+			path: "/add-listing",
+			element: <AddListing />,
+		},
+		{
+			path: "/edit-profile",
+			element: <EditProfile />,
+		},
+		{
+			path: "/explore",
+			element: <Explore />,
+		},
+	]);
+	return <RouterProvider router={router} />;
 }
