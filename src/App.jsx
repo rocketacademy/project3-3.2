2774bc789448db8a9fd73ee5b593c03b6dd8e078
Chxshire22 <akuindra22@gmail.com>
2024-02-05 11:07:42 +0800

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
-    <>
-    </>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
