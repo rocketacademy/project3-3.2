271ef4a861f6a918e287f5f6272babb54c8e1648
Chxshire22 <akuindra22@gmail.com>
2024-02-06 01:15:35 +0800

    found out how to implement users' styles

--- src/Components/Prototyping.jsx
+++ src/Components/Prototyping.jsx
@@ -1,34 +1,54 @@
 import { useNavigate } from "react-router-dom";
 
-export default function Prototyping(props) {
+export default function Prototyping() {
   const navigate = useNavigate();
 
-	let { createdAt, content, senderUsername,} = props
-	// let { senderId, currentUserId, createdAt, content} = props
-
-	let senderId = 1
-	let currentUserId = 1
   return (
-    <>
-      {/* MAP LIST OF MESSAGES */}
 
-      <div
-        className={
-          "chat" + (senderId == currentUserId ? " chat-end" : " chat-start")
-        }
-      >
-        <div onClick={()=>`/profile/${senderId}`} className="chat-image avatar cursor-pointer">
-          <div className="w-10 rounded-full">
-            <img
-              alt="Tailwind CSS chat bubble component"
-              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            />
+    <>
+      {/* <style>
+        {`.test{
+    		background-color:red;
+    	}`}
+      </style> */}
+      <div className="h-screen mx-4 mt-2">
+        <header className="mx-4 mt-2">
+          <div className="h-10 w-full flex flex-row items-center">
+            <div
+              onClick={() => navigate(`/`)}
+              className="flex flex-row items-center mb-2 px-4 bg-[#83C0C1] h-full rounded-full  cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="white"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>{" "}
+              <h2 className="font-bold ml-4 flex-1 text-white test">Back</h2>
+            </div>{" "}
           </div>
+          <hr />
+        </header>
+        <div className="bg-[#83C0C1]/50 flex justify-center items-center rounded size-40">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-20 h-20"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
         </div>
-        <div className="chat-header">
-          <time className="text-xs opacity-50">{createdAt}</time>
-        </div>
-        <div className="chat-bubble bg-slate-200">{content? content: "MESSAGES"}</div>
       </div>
     </>
   );
