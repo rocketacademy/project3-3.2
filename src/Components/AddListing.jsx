27574f6c83e3d2cd30b3db600043dfb659793091
Chxshire22 <akuindra22@gmail.com>
2024-02-10 21:35:48 +0800

    added code to verify data we sending to backend

--- src/Components/AddListing.jsx
+++ src/Components/AddListing.jsx
@@ -7,190 +7,196 @@ import axios from "axios";
 import { BACKEND_URL } from "./lib/constants";
 
 export default function AddListing() {
-  const [preview, setPreview] = useState([]);
-  const [selectedImage, setSelectedImage] = useState([]);
+	const [preview, setPreview] = useState([]);
+	const [selectedImage, setSelectedImage] = useState([]);
 	// Data from backend
-  const [categories, setCategories] = useState([])
-  // Data for backend
-  const [category, setCategory] = useState(null);
-  const [listingTitleValue, setListingTitleValue] = useState("");
-  const [priceValue, setPriceValue] = useState("");
-  const [descriptionValue, setDescriptionValue] = useState("");
+	const [categories, setCategories] = useState([]);
+	// Data for backend
+	const [dropdownSelect, setDropdownSelect] = useState(null);
+	const [listingTitleValue, setListingTitleValue] = useState("");
+	const [priceValue, setPriceValue] = useState("");
+	const [descriptionValue, setDescriptionValue] = useState("");
 
+	const navigate = useNavigate();
 
-  const navigate = useNavigate();
+	useEffect(() => {
+		window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+	}, []);
 
-  useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  }, []);
+	const getCategories = async () => {
+		const allCategories = await axios.get(`${BACKEND_URL}/categories`);
+		setCategories(allCategories.data);
+	};
+	useEffect(() => {
+		getCategories();
+	}, []);
 
-  // ONCE BACKEND ROUTES AND CONTROLLERS ARE FILLED, GET REQUEST TO ADD IN OPTIONS FROM CATEGORIES TABLE
-  const getCategories = async() => {
-    const allCategories = await axios.get(`${BACKEND_URL}/categories`)
-    setCategories(allCategories.data)
-  }
-  useEffect(()=>{
-    getCategories()
-  },[])
+	// Dropdown logic
+	const options = categories.map((category) => ({
+		value: category.id,
+		label: category.name,
+	}));
 
-  useEffect(()=>{
-    console.log(categories)
-  },[categories])
+	// Image handling
+	const handleChange = (value) => {
+		console.log("value:", value);
+		setDropdownSelect(value);
+	};
+	const handleImageChange = (e) => {
+		if (!e.target.files || e.target.files.length === 0) {
+			setSelectedImage(null);
+			return;
+		}
+		setSelectedImage(e.target.files);
+	};
 
-  const options = categories.map((category)=>({value:category.id, label:category.name}))
+	useEffect(() => {
+		if (selectedImage?.length !== 0) {
+			const localUrls = [];
+			for (let file in selectedImage) {
+				if (selectedImage[file] instanceof File) {
+					localUrls.push(URL.createObjectURL(selectedImage[file]));
+				}
+			}
+			setPreview(localUrls);
+		}
+	}, [selectedImage]);
 
-  const handleChange = (value) => {
-    console.log("value:", value);
-    setCategory(value);
-  };
-  const handleImageChange = (e) => {
-    console.log(e.target.files);
-    if (!e.target.files || e.target.files.length === 0) {
-      setSelectedImage(null);
-      return;
-    }
-    setSelectedImage(e.target.files);
-  };
+	useEffect(() => {
+		console.log(dropdownSelect?.value);
+		console.log("price", priceValue);
+		console.log("listing title", listingTitleValue);
+		console.log("description", descriptionValue);
+		console.log("selectedImage", selectedImage);
+	}, [
+		priceValue,
+		listingTitleValue,
+		descriptionValue,
+		dropdownSelect,
+		selectedImage,
+	]);
 
-  useEffect(() => {
-    if (selectedImage?.length !== 0) {
-      const localUrls = [];
-      for (let file in selectedImage) {
-        if (selectedImage[file] instanceof File) {
-          localUrls.push(URL.createObjectURL(selectedImage[file]));
-        }
-      }
-      setPreview(localUrls);
-    }
-  }, [selectedImage]);
+	return (
+		<>
+			<div className="h-screen mx-4 mt-2 ">
+				<header className="mx-4 mt-2 mb-4">
+					<div className="h-10 w-full flex flex-row items-center">
+						<div
+							onClick={() => navigate(-1)}
+							className="flex flex-row items-center mb-2 px-4 bg-[#83C0C1] h-full rounded-full  cursor-pointer"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="white"
+								className="w-6 h-6"
+							>
+								<path
+									fillRule="evenodd"
+									d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+									clipRule="evenodd"
+								/>
+							</svg>{" "}
+							<h2 className="font-bold ml-4 flex-1 text-white test">Back</h2>
+						</div>{" "}
+					</div>
+					<hr />
+				</header>
+				{/* IMAGE CAROUSEL */}
+				{preview.length !== 0 ? (
+					<Carousel imgArr={preview} />
+				) : (
+					<div className="bg-[#83C0C1]/50 flex flex-col justify-center items-center rounded w-full aspect-[4/5] mx-auto">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							className="w-20 h-20"
+						>
+							<path
+								fillRule="evenodd"
+								d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+								clipRule="evenodd"
+							/>
+						</svg>
+						<p className="opacity-60 text-xs font-medium">Image Preview</p>
+					</div>
+				)}
 
-  useEffect(() => {
-    console.log("price", priceValue);
-    console.log("listing title", listingTitleValue);
-    console.log("description", descriptionValue);
-  }, []);
+				{/* IMAGE INPUT */}
 
-  return (
-    <>
-      <div className="h-screen mx-4 mt-2 ">
-        <header className="mx-4 mt-2 mb-4">
-          <div className="h-10 w-full flex flex-row items-center">
-            <div
-              onClick={() => navigate(-1)}
-              className="flex flex-row items-center mb-2 px-4 bg-[#83C0C1] h-full rounded-full  cursor-pointer"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="white"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>{" "}
-              <h2 className="font-bold ml-4 flex-1 text-white test">Back</h2>
-            </div>{" "}
-          </div>
-          <hr />
-        </header>
-        {/* IMAGE CAROUSEL */}
-        {preview.length !== 0 ? (
-          <Carousel imgArr={preview} />
-        ) : (
-          <div className="bg-[#83C0C1]/50 flex flex-col justify-center items-center rounded w-full aspect-[4/5] mx-auto">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-20 h-20"
-            >
-              <path
-                fillRule="evenodd"
-                d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <p className="opacity-60 text-xs font-medium">Image Preview</p>
-          </div>
-        )}
+				<input
+					type="file"
+					multiple
+					accept="image/*"
+					className="file-input w-full max-w-xs mt-4"
+					onChange={handleImageChange}
+					required
+					maxLength={10}
+				/>
 
-        {/* IMAGE INPUT */}
+				{/* DROPDOWN */}
+				<div className="mt-4">
+					<Select
+						value={dropdownSelect}
+						onChange={handleChange}
+						options={options}
+						isSearchable={true}
+						required
+					/>
+				</div>
+				{/* REST OF FORM FIELDS */}
+				<div className="flex flex-col justify-evenly items-start">
+					<input
+						type="text"
+						placeholder="Listing Title"
+						className="w-full mt-4 p-3 bg-slate-300/30 rounded outline-[#83C0C1] active:outline-[#83C0C1]"
+						required
+						maxLength={35}
+						onChange={(e) => setListingTitleValue(e.target.value)}
+					/>
+					{/* TODO: NEED SOME INPUT VALIDATION */}
+					<CurrencyInput
+						className="mt-4 p-3 bg-slate-300/30 rounded outline-[#83C0C1] active:outline-[#83C0C1]"
+						id="input-example"
+						name="input-name"
+						placeholder="Price"
+						prefix="$"
+						onValueChange={(value, name, values) => {
+							console.log(value, name, values);
+							setPriceValue(value);
+						}}
+						maxLength={5}
+					/>
 
-        <input
-          type="file"
-          multiple
-          accept="image/*"
-          className="file-input w-full max-w-xs mt-4"
-          onChange={handleImageChange}
-          required
-          maxLength={10}
-        />
-
-        {/* DROPDOWN */}
-        <div className="mt-4">
-          <Select
-            value={category}
-            onChange={handleChange}
-            options={options}
-            isSearchable={true}
-            required
-          />
-        </div>
-        {/* REST OF FORM FIELDS */}
-        <div className="flex flex-col justify-evenly items-start">
-          <input
-            type="text"
-            placeholder="Listing Title"
-            className="w-full mt-4 p-3 bg-slate-300/30 rounded outline-[#83C0C1] active:outline-[#83C0C1]"
-            required
-            maxLength={35}
-            onChange={(e) => setListingTitleValue(e.target.value)}
-          />
-          {/* TODO: NEED SOME INPUT VALIDATION */}
-          <CurrencyInput
-            className="mt-4 p-3 bg-slate-300/30 rounded outline-[#83C0C1] active:outline-[#83C0C1]"
-            id="input-example"
-            name="input-name"
-            placeholder="Price"
-            prefix="$"
-            onValueChange={(value, name, values) => {
-              console.log(value, name, values);
-              setPriceValue(values);
-            }}
-            maxLength={5}
-          />
-
-          <textarea
-            placeholder="Description"
-            name=""
-            id=""
-            cols="30"
-            rows="10"
-            className="w-full mt-4 p-3 outline-[#83C0C1] rounded active:outline-[#83C0C1] bg-slate-300/30"
-            required
-            onChange={(e) => setDescriptionValue(e.target.value)}
-          ></textarea>
-        </div>
-        <hr />
-        <div className="flex flex-row items-center justify-center mt-4 mb-4">
-          <button
-            onClick={() => navigate("/preview-listing")}
-            className="btn w-full bg-[#83C0C1] text-white text-lg relative bottom-0 hover:opacity-100 transition ease-in mb-4 "
-            disabled={
-              selectedImage.length == 0 ||
-              listingTitleValue.length == 0 ||
-              priceValue == 0
-                ? true
-                : false
-            }
-          >
-            Save and Continue
-          </button>
-        </div>
-      </div>
-    </>
-  );
+					<textarea
+						placeholder="Description"
+						name=""
+						id=""
+						cols="30"
+						rows="10"
+						className="w-full mt-4 p-3 outline-[#83C0C1] rounded active:outline-[#83C0C1] bg-slate-300/30"
+						required
+						onChange={(e) => setDescriptionValue(e.target.value)}
+					></textarea>
+				</div>
+				<hr />
+				<div className="flex flex-row items-center justify-center mt-4 mb-4">
+					<button
+						onClick={() => navigate("/preview-listing")}
+						className="btn w-full bg-[#83C0C1] text-white text-lg relative bottom-0 hover:opacity-100 transition ease-in mb-4 "
+						disabled={
+							selectedImage.length == 0 ||
+							listingTitleValue.length == 0 ||
+							priceValue == 0
+								? true
+								: false
+						}
+					>
+						Save and Continue
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
