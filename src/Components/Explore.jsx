9cf13968510f23a977e8447dd093d00b7232066a
Chxshire22 <akuindra22@gmail.com>
2024-02-13 17:47:20 +0800

    categories now show up on explore page

--- src/Components/Explore.jsx
+++ src/Components/Explore.jsx
@@ -1,7 +1,22 @@
 import Navbar from "./UiComponents/Navbar";
 import Search from "./UiComponents/SearchBar";
+import axios from "axios";
+import { BACKEND_URL } from "./lib/constants";
+import { useEffect, useState } from "react";
+
 export default function Explore() {
+  const [categories, setCategories] = useState([]);
   const searchType = "Search";
+
+  const getCategories = async () => {
+    const categories = await axios.get(`${BACKEND_URL}/categories`);
+    setCategories(categories.data);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
     <>
       <div className="h-screen mx-4 mt-4">
@@ -9,32 +24,21 @@ export default function Explore() {
           <Search searchType={searchType} />
         </div>
         <hr />
-        <h2 className="font-semibold text-lg text-black/80 mt-4 text-center">Categories</h2>
+        <h2 className="font-semibold text-lg text-black/80 mt-4 text-center">
+          Categories
+        </h2>
 
-{/* GET LIST OF CATEGORIES AND MAP THEM HERE */}
+        {/* GET LIST OF CATEGORIES AND MAP THEM HERE */}
 
         <div className="flex flex-row items-center justify-center flex-wrap w-full gap-2 mt-2">
-          <button className=" bg-[#6C22A6]/60 text-white outline-none border-none opacity-80 hover:opacity-100 transition ease-in py-1 px-2 rounded-full">
-            Blender
-          </button>
-          <button className=" bg-[#6C22A6]/60 text-white outline-none border-none  opacity-80 hover:opacity-100 transition ease-in py-1 px-2 rounded-full">
-            Crochet
-          </button>
-          <button className=" bg-[#6C22A6]/60 text-white outline-none border-none  opacity-80 hover:opacity-100 transition ease-in py-1 px-2 rounded-full">
-            Oil Painting
-          </button>
-          <button className=" bg-[#6C22A6]/60 text-white outline-none border-none  opacity-80 hover:opacity-100 transition ease-in py-1 px-2 rounded-full">
-            Embroidery
-          </button>
-          <button className=" bg-[#6C22A6]/60 text-white outline-none border-none  opacity-80 hover:opacity-100 transition ease-in py-1 px-2 rounded-full">
-            Photography
-          </button>
-          <button className=" bg-[#6C22A6]/60 text-white outline-none border-none  opacity-80 hover:opacity-100 transition ease-in py-1 px-2 rounded-full">
-            Pottery
-          </button>
-          <button className=" bg-[#6C22A6]/60 text-white outline-none border-none  opacity-80 hover:opacity-100 transition ease-in py-1 px-2 rounded-full">
-            UI Design
-          </button>
+          {categories.map((category) => (
+            <button
+              key={category.id}
+              className=" bg-[#6C22A6]/60 text-white outline-none border-none opacity-80 hover:opacity-100 transition ease-in py-1 px-2 rounded-full"
+            >
+              {category.name}
+            </button>
+          ))}
         </div>
         <Navbar />
       </div>
